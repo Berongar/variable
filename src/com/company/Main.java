01aa43f9bbedf0a067212d6fdf5ba0492a90f5ce
Berogar <Andropov.M.V@gmail.com>
2020-11-26 15:17:05 +0300

    Простой пример по работе с переменными

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int var1; //объявляем переменную 1
+        int var2; //обьявляем переменную 2
+
+        var1 = 1024; //присваиваем значение переменной
+
+        System.out.println("Переменная var1 содержит: " + var1); //Выводим значение первой переменной
+
+        var2 = var1/2; // выполняем арифметическое действие
+
+        System.out.print("Переменная var2 содержит var1/2: ");
+        System.out.println(var2);
+    }
+}
